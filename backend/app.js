ac0d12583f4edb4ab9add5c640b2739521a0ee29
maru33luc <maru33luc@gmail.com>
2024-01-07 20:20:47 -0300

    [FIX] port environment app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 const session = require('express-session');
