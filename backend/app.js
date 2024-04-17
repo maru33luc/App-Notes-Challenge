07934747ec3acf8f868858b3a68deed855510de5
maru33luc <maru33luc@gmail.com>
2024-04-16 21:01:41 -0300

    Update app.js and db.js for SSL support

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-// const port = 3040;
+
 const cors = require('cors');
 
 const session = require('express-session');
--- backend/data/db.js
+++ backend/data/db.js
@@ -4,7 +4,15 @@ require('dotenv').config();
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    schema: 'public',
+    ssl: true,  // Habilitar SSL
+    dialectOptions: {
+        ssl: {
+            require: true,  // Requerir SSL
+            rejectUnauthorized: false  // Deshabilitar la verificación del certificado (si es necesario)
+        }
+    }
 });
 
 module.exports = db;
