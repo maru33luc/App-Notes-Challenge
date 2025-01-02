8141b3bfd89723d255a141a3534455f3c218487a
maru33luc <maru33luc@gmail.com>
2025-01-02 09:39:16 -0300

    Update router configuration to enable blocking initial navigation

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation()), provideClientHydration()]
+  providers: [provideRouter(routes, withEnabledBlockingInitialNavigation()), provideClientHydration()]
 };
