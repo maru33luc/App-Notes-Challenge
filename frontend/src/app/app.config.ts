7c17c64d7e5074b31f07e98cd7ec6f656ec602ec
maru33luc <maru33luc@gmail.com>
2025-01-02 09:36:40 -0300

    Update app configuration to use hash location strategy for routing

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,8 +3,7 @@ import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes, withHashLocation()), provideClientHydration()]
 };
